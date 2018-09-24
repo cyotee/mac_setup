1cebaf339b6b0a0fb8a26d0b8ecefa8d0b4c535b
Robert Greathouse <cyotee@gmail.com>
2018-09-24 12:41:23 -0700

    Added Pathfinder

--- setup.sh
+++ setup.sh
@@ -20,5 +20,8 @@ brew cask install vlc
 brew cask install gog-galaxy
 brew cask isntall steam
 brew cask install tunnelblick
+brew cask install gitkraken
+brew cask install path-finder
+
 brew cask install private-internet-access
 open -a "/usr/local/Caskroom/private-internet-access/81/Private Internet Access Installer.app"
