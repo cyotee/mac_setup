37066a9481fbfa406b77b59d5c62112e47084434
Robert Greathouse <cyotee@gmail.com>
2018-09-24 06:59:58 -0700

    Updated

--- setup.sh
+++ setup.sh
@@ -5,15 +5,19 @@
 brew tap jetbrains/utils
 brew install caskroom/cask/brew-cask
 
+brew cask install java
+brew install go --cross-compile-common
 brew cask install iterm2
 brew cask install atom
 brew cask install virtualbox virtualbox-extension-pack
-
 brew cask install docker
 brew cask install jetbrains-toolbox
-brew cask install vlc
-brew cask install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt
-brew cask install go
-brew cask install node
-
 brew cask install vagrant vagrant-manager
+
+brew cask install google-chrome
+brew cask install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt
+brew cask install vlc
+brew cask install node
+brew cask install gog-galaxy
+
+
